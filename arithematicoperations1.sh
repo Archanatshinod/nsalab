b91a129e67d69f6bc861e081a0a0a979071edfa5
Archanatshinod <116487133+Archanatshinod@users.noreply.github.com>
2023-06-08 11:45:42 +0530

    Add files via upload

---
+++ arithematicoperations1.sh
@@ -0,0 +1,16 @@
+echo "enter first number:"
+read a
+echo "enter second number:"
+read b
+sum=$((a+b))
+echo "sum is "
+echo $sum
+diff=$((a-b))
+echo "difference is "
+echo $diff
+pro=$((a*b))
+echo "product is"
+echo $pro
+quo=$((a/b))
+echo "quotient:"
+echo $quo
---
+++ message1.sh
@@ -0,0 +1,4 @@
+echo "enter a message: "
+read msg
+echo $msg
+
